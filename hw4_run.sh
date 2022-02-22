7a9dfc33aa06c8c36c0098e65325c012d9212b19
yhlin <oscarlin2008@gmail.com>
2022-02-21 16:01:42 -0800

    chg: dev: add the hw4 script
    hw4_run.sh: running several processes at the same time and generate
    all the combinations of blocking_type and cache_config.

---
+++ hw4_run.sh
@@ -0,0 +1,10 @@
+for (( b=0; b<=4; b++ ))
+do
+	for (( c=0; c<=3; c++ ))
+    	do
+
+        	now=$(date +%s)
+        	../gem5/build/RISCV/gem5.opt --outdir=m5out/${now}_${b}_${c} run.py $b $c &
+    	done
+done
+
